f829a0180e988690026c65f43bebce3d1e0d3d67
insyirah <insyirah@resolvtech.net>
2017-11-20 16:24:32 +0800

    z

--- src/pages/treatmentproviders/treatmentproviders.module.ts
+++ src/pages/treatmentproviders/treatmentproviders.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TreatmentprovidersPage } from './treatmentproviders';
 import {NguiMapModule} from '@ngui/map';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
   declarations: [
